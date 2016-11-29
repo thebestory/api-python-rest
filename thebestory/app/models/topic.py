12d4fcc98c0c547e1e7ed945ca326dc2cf3b7dc6
Nariman Safiulin <woofilee@gmail.com>
2016-11-29 22:13:27 +0300

    Topic's slug must be unique

--- thebestory/app/models/topic.py
+++ thebestory/app/models/topic.py
@@ -12,8 +12,8 @@ table = sa.Table(
 
     sa.Column("id", sa.Integer, primary_key=True),
 
+    sa.Column("slug", sa.String(32), unique=True, nullable=False),
     sa.Column("title", sa.String(64), nullable=False),
-    sa.Column("slug", sa.String(32), nullable=False),
 
     sa.Column("description", sa.Text, nullable=False),
     sa.Column("icon", sa.String(16), nullable=False),
