fac7e19866a1399b7456e8e1361c4f9233991d56
Nariman Safiulin <woofilee@gmail.com>
2016-12-01 02:59:37 +0300

    Topics controller done

--- thebestory/app/controllers/api/topics.py
+++ thebestory/app/controllers/api/topics.py
@@ -4,11 +4,14 @@ The Bestory Project
 
 import json
 from aiohttp import web
+from sqlalchemy.sql import select
 
 from thebestory.app.lib import identifier, listing
-from thebestory.app.models import table, topics
+from thebestory.app.lib.api.response import *
+from thebestory.app.models import stories, topics
 
 
+# TODO: In next API, upgrade to ``slug`` only public IDs (but save internal IDs)
 class TopicsController:
     # 25 stories per page
     listing = listing.Listing(1, 100, 25)
@@ -20,19 +23,32 @@ class TopicsController:
         try:
             id = int(request.match_info["id"])
         except KeyError:
-            return web.Response(status=400, content_type='application/json')
+            return web.Response(
+                status=400,
+                content_type="application/json",
+                text=json.dumps(error(2002)))
 
         async with request.db.acquire() as conn:
             topic = await conn.fetchrow(
-                topics.select().where(topics.c.id == id))
+                select([topics]).where(topics.c.id == id))
 
-            if topic.row is None:
-                return web.Response(status=404, content_type='application/json')
+        if topic.row is None:
+            return web.Response(
+                status=404,
+                content_type="application/json",
+                text=json.dumps(error(2002)))
 
         return web.Response(
             status=200,
-            content_type='application/json',
-            text=json.dumps(self._topic(topic)))
+            content_type="application/json",
+            text=json.dumps(ok({
+                "id": topic.id,
+                "slug": topic.slug,
+                "title": topic.title,
+                "icon": topic.icon,
+                "description": topic.description,
+                "stories_count": topic.stories_count
+            })))
 
     async def list(self, request):
         """
@@ -41,14 +57,21 @@ class TopicsController:
         data = []
 
         async with request.db.acquire() as conn:
-            for row in await conn.fetch(
-                    topics.select().order_by(topics.c.slug.desc())):
-                data.append(self._topic(row))
+            for topic in await conn.fetch(
+                    select([topics]).order_by(topics.c.slug.desc())):
+                data.append({
+                    "id": topic.id,
+                    "slug": topic.slug,
+                    "title": topic.title,
+                    "icon": topic.icon,
+                    "description": topic.description,
+                    "stories_count": topic.stories_count
+                })
 
         return web.Response(
             status=200,
-            content_type='application/json',
-            text=json.dumps(data))
+            content_type="application/json",
+            text=json.dumps(ok(data)))
 
     # TODO: Before and After with publish dates, not IDs
     async def stories(self, request):
@@ -60,86 +83,58 @@ class TopicsController:
             id = int(request.match_info["id"])
             pivot, limit, direction = self.listing.validate(
                 request.url.query.get("before", None),
-                request.url.query.get("around", None),
                 request.url.query.get("after", None),
-                request.url.query.get("limit", None))
+                request.url.query.get("limit", None)
+            )
         except (KeyError, ValueError):
-            return web.Response(status=400, content_type='application/json')
+            return web.Response(
+                status=400,
+                content_type="application/json",
+                text=json.dumps(error(2002)))
 
         data = []
-        query = table.select().where(table.c.topic_id == id).order_by(
-            table.c.publish_date.desc())
 
         async with request.db.acquire() as conn:
             topic = await conn.fetchrow(
-                topics.select().where(topics.c.id == id))
+                select([topics]).where(topics.c.id == id))
 
             if topic.row is None:
-                return web.Response(status=404, content_type='application/json')
+                return web.Response(
+                    status=404,
+                    content_type="application/json",
+                    text=json.dumps(error(2002)))
 
-            if direction != listing.Direction.AROUND:
-                query = query.limit(limit)
+            query = select([
+                stories.t.c.id,
+                stories.t.c.content,
+                stories.t.c.likes_count,
+                stories.t.c.comments_count,
+                stories.t.c.edited_date,
+                stories.t.c.published_date
+            ]) \
+                .where(topics.c.id == topic.id) \
+                .where(stories.c.is_approved is True) \
+                .where(stories.c.is_removed is False) \
+                .order_by(stories.t.c.published_date.desc())
 
-                # if pivot is none, fetch first page w/o any parameters
-                if pivot is not None:
-                    if direction == listing.Direction.BEFORE:
-                        query = query.where(table.c.id < pivot)
-                    elif direction == listing.Direction.AFTER:
-                        query = query.where(table.c.id > pivot)
+            # if pivot is none, fetch first page w/o any parameters
+            if pivot is not None:
+                if direction == listing.Direction.AFTER:
+                    query = query.where(stories.c.id < pivot)
+                elif direction == listing.Direction.BEFORE:
+                    query = query.where(stories.c.id > pivot)
 
-                for row in await conn.fetch(query):
-                    data.append(self._story(row, topic))
-            else:
-                # TODO: check if only part data was fetched
-                query_before = query.where(table.c.id <= pivot).limit(
-                    sum(divmod(limit, 2)))
-                query_after = query.where(table.c.id > pivot).limit(
-                    limit // 2)
-
-                query = query_before.union(query_after)
-
-                for row in await conn.fetch(query):
-                    data.append(self._story(row, topic))
+            for story in await conn.fetch(query):
+                data.append({
+                    "id": identifier.to36(story.id),
+                    "content": story.content,
+                    "likes_count": story.likes_count,
+                    "comments_count": story.comments_count,
+                    "edited_date": story.edited_date,
+                    "published_date": story.published_date
+                })
 
         return web.Response(
             status=200,
-            content_type='application/json',
-            text=json.dumps(data))
-
-    @staticmethod
-    def _topic(topic):
-        data = dict()
-
-        data["id"] = topic.id
-        data["slug"] = topic.slug
-        data["title"] = topic.title
-        data["description"] = topic.desc
-        data["icon"] = topic.icon
-        data["stories_count"] = topic.stories_count
-
-        return data
-
-    @staticmethod
-    def _story(story, topic):
-        data = dict()
-
-        data["id"] = identifier.to36(story.id)
-
-        data["topic"] = dict()
-        data["topic"]["id"] = topic.id
-        data["topic"]["slug"] = topic.slug
-        data["topic"]["title"] = topic.title
-
-        data["content"] = story.content
-
-        data["likes_count"] = 0
-        data["comments_count"] = 0
-
-        data["submit_date"] = story.submit_date.isoformat()
-
-        if story.publish_date is not None:
-            data["publish_date"] = story.publish_date.isoformat()
-        else:
-            data["publish_date"] = None
-
-        return data
+            content_type="application/json",
+            text=json.dumps(ok(data)))
