4f3fe1e813463d6e1c130fae5267b543198bbb22
Nariman Safiulin <woofilee@gmail.com>
2017-06-03 01:09:27 +0300

    Users controller methods implementation

--- tbs/controllers/users.py
+++ tbs/controllers/users.py
@@ -4,14 +4,39 @@ The Bestory Project
 
 from sanic.response import json
 
+from tbs import db
+from tbs.lib import exceptions
+from tbs.lib import response_wrapper
+from tbs.lib.stores import user as user_store
+from tbs.views import user as user_view
+
 
 async def create_user(request):
-    return json({"hello": "world"})
+    user = request.json
+
+    with db.pool.acquire() as conn:
+        user = user_store.create(
+            conn=conn,
+            username=user["username"],
+            email=user["email"],
+            password=user["password"]
+        )
+
+        return json(response_wrapper.ok(user_view.render(user)))
 
 
 async def show_user(request, id):
-    return json({"hello": "world"})
+    try:
+        with db.pool.acquire() as conn:
+            user = user_store.get(conn=conn, id=id)
+            return json(response_wrapper.ok(user_view.render(user)))
+    except exceptions.NotFoundError:
+        return json(response_wrapper.error(4001), status=404)
 
 
 async def update_user(request, id):
-    return json({"hello": "world"})
+    user = request.json
+
+    with db.pool.acquire() as conn:
+        user = user_store.update(conn=conn, id=id, **user)
+        return json(response_wrapper.ok(user_view.render(user)))
--- tbs/views/user.py
+++ tbs/views/user.py
@@ -7,8 +7,10 @@ def render(user):
     return {
         "id": user["id"],
         "username": user["username"],
+        "email": user["email"],
         "comments_count": user["comments_count"],
-        "likes_count": user["likes_count"],
+        "comment_likes_count": user["comment_reactions_count"],
+        "story_likes_count": user["story_reactions_count"],
         "stories_count": user["stories_count"],
         "registered_date": user["registered_date"].isoformat()
     }
