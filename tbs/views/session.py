cee8bde1987c0305c642cda0b96a05c554d9ca1f
Nariman <woofilee@gmail.com>
2017-06-03 14:41:34 +0300

    Session renderer fix

--- tbs/views/session.py
+++ tbs/views/session.py
@@ -8,14 +8,5 @@ from tbs.views import user as user_view
 def render(session):
     return {
         "jwt": session["jwt"],
-        "user": render_user(session["user_id"], session["user"]),
-        "created_date": session["created_date"].isoformat(),
-        "expired_date": session["expired_date"].isoformat()
+        "user": user_view.render(session["user"])
     }
-
-
-def render_user(user_id, user=None):
-    if user is None:
-        return {"id": user_id}
-    else:
-        return user_view.render(user)
