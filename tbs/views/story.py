97b409553458395b7703b0c99ccff91cf8b96fbe
Nariman <woofilee@gmail.com>
2017-06-03 16:28:03 +0300

    Story view changes

--- tbs/views/story.py
+++ tbs/views/story.py
@@ -9,8 +9,8 @@ from tbs.views import topic as topic_view
 def render(story, author, topic=None):
     return {
         "id": story["id"],
-        "author": render_author(story["author_id"], author),
-        "topic": render_topic(story["topic"], topic),
+        "author": user_view.render(author),
+        "topic": topic_view.render(topic) if topic is not None else None,
         "content": story["content"],
         "comments_count": story["comments_count"],
         "likes_count": story["likes_count"],
@@ -21,20 +21,6 @@ def render(story, author, topic=None):
         "published_date": render_date(story["published_date"]),
         "edited_date": render_date(story["edited_date"])
     }
-
-
-def render_author(author_id, author=None):
-    if author is None:
-        return {"id": author_id}
-    else:
-        return user_view.render(author)
-
-
-def render_topic(topic_id, topic=None):
-    if topic is None:
-        return {"id": topic_id}
-    else:
-        return topic_view.render(topic)
 
 
 def render_date(date):
